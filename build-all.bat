e782e4777e7b73f2ac3d6dcf88d3edd0c66610b7
Mike Chabot <mchabot@gmail.com>
2013-11-24 12:52:43 -0500

    Removing some Oracle jars (connected to Oracle Express quickly for s&gs. Also removed from SLF4J jars that were colliding

--- build-all.bat
+++ build-all.bat
@@ -1,4 +1,7 @@
 net stop ApacheTomcat7
 RD /S /Q "D:/Development/apache-tomcat-7.0.34/webapps/
+RD /S /Q "D:/Development/apache-tomcat-7.0.34/work/
+MKDIR "D:/Development/apache-tomcat-7.0.34/webapps/
 call build-war.bat
+copy "D:\Development\workspaces\AnotherGlass\AnotherGlass\build\ROOT.war" "D:\Development\apache-tomcat-7.0.34\webapps"
 net start ApacheTomcat7
